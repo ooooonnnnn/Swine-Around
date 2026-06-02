7ba7e7e4758240f9f367b97197a8252080afd665
CutebatDev <alexaverten@gmail.com>
2026-06-02 18:09:19 +0300

    Not done, just saving
    CAREFUL!!!! CHANGING: FoodPrefab, FoodScript

---
+++ Assets/Scripts/Gameplay/Food/FoodObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Food
+{
+    public class FoodObject : MonoBehaviour
+    {
+        // private void Awake()
+        // {
+        //     if (!LevelReseting.Instance)
+        //         return;
+        //     if (!LevelReseting.Instance.AddToDoNotDestroy(gameObject))
+        //     {
+        //         Destroy(gameObject);
+        //     }
+        //     Debug.Log($"Object {gameObject.name} added to DND");
+        // }
+        
+        
+        // this idea did not work... try mapping all food with this bool
+        // isEaten = false;
+        // and just not spawn it if it is eaten
+
+        // private void Awake()
+        // {
+        //     if (isEaten)
+        //         Destroy(gameObject);
+        // }
+
+     
+    }
+}
