a9fd6ad4a8ba9ad6e51e8a1cc1766f771085a39a
CutebatDev <alexaverten@gmail.com>
2026-06-04 22:19:58 +0300

    Score popup script + prefab

---
+++ Assets/Scripts/Gameplay/Effects/ScorePopup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+namespace Gameplay.Effects
+{
+    
+    // just instantiate this in right position, and set its parameters (just score is enough)
+    
+    public class ScorePopup : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI tmpAsset;
+        
+        public int score = 999;
+        public float popUpDuration = 2.0f;
+        public float upMovementDistance = 3.0f;
+        
+        private void Awake()
+        {
+            tmpAsset.text = $"+{score}";
+            
+            Sequence seq = DOTween.Sequence();
+
+            seq.Join(transform.DOMove(transform.position + Vector3.up * upMovementDistance, popUpDuration));
+            seq.Join(tmpAsset.DOFade(0f, popUpDuration));
+
+            seq.OnComplete(() => Destroy(gameObject));
+        }
+        
+    }
+}
