b796761992ab4149cce21cf4d2f0852bc0801a11
CutebatDev <alexaverten@gmail.com>
2026-06-08 14:42:41 +0300

    fix for the bug + meta file

--- Assets/Scripts/Shaders/OutlineShader.cs
+++ Assets/Scripts/Shaders/OutlineShader.cs
@@ -2,37 +2,52 @@ using UnityEngine;
 
 [ExecuteAlways]
 [RequireComponent(typeof(Renderer))]
-public class OutlineShader : MonoBehaviour
+public class OutlineScript : MonoBehaviour
 {
-    public float outlineWidth = 0.1f;
+    public float outline = 1.1f;
     public Color color = Color.red;
 
     private Renderer _renderer;
     private MaterialPropertyBlock _mpb;
-    
-    void OnEnable()
+
+    private float _lastOutline;
+    private Color _lastColor;
+
+    void Awake()
     {
         _renderer = GetComponent<Renderer>();
         _mpb = new MaterialPropertyBlock();
         Apply();
     }
 
-    void OnValidate()
+    void Update()
     {
-        if (!Application.isPlaying)
+        if (_lastOutline != outline || _lastColor != color)
             Apply();
     }
 
-    void Apply()
+    private void Apply()
     {
-        if (_renderer == null)
+        if (_renderer)
             _renderer = GetComponent<Renderer>();
 
         if (_mpb == null)
             _mpb = new MaterialPropertyBlock();
 
         _renderer.GetPropertyBlock(_mpb);
-        _mpb.SetFloat("_Outline_Scale", outlineWidth + 1.0f);
+
+        _mpb.SetFloat("_Outline_Scale", outline);
+        _mpb.SetColor("_Outline_Color", color);
+
+        _renderer.SetPropertyBlock(_mpb);
+    }
+    
+    public void SetOutline(float scale, Color color)
+    {
+        outline = scale;
+
+        _renderer.GetPropertyBlock(_mpb);
+        _mpb.SetFloat("_Outline_Scale", scale);
         _mpb.SetColor("_Outline_Color", color);
         _renderer.SetPropertyBlock(_mpb);
     }
