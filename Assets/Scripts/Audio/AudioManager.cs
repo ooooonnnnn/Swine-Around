ca3ae75a130e726522980bfda8d89d5b78c6ac74
CutebatDev <alexaverten@gmail.com>
2026-06-16 16:11:13 +0300

    local save

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null)
+            Debug.LogError("More than one audioManager");
+        Instance = this;
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+    
+}
---
+++ Assets/Scripts/Audio/FoodSound.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class FoodSound : MonoBehaviour
+{
+    public void CollectFood()
+    {
+        // AudioManager.Instance.PlayOneShot();
+    }
+}
