fd0ba9196a137f16296499699cd4384e5da68dbf
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-06-26 20:05:43 +0300

    blocking restart once checkpoint is activated (can't get caught)

--- Assets/Scripts/Gameplay/Managers/GameManager.cs
+++ Assets/Scripts/Gameplay/Managers/GameManager.cs
@@ -1,18 +1,22 @@
 using Gameplay;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class GameManager : PersistentSingleton<GameManager>
 {
     [SerializeField] private float delayBeforeCheckpointRestart;
     [SerializeField] private UnityEvent OnLoseMatch;
     [SerializeField] private UnityEvent OnRestartMatch;
-    
+    private bool _canRestart = true;
+
     /// <summary>
     /// When you get caught and lose a life
     /// </summary>
     public void LoseMatch()
     {
+        if (!_canRestart) return;
+        
         OnLoseMatch.Invoke();
         LevelStateManager.Instance.ResetLevel();
         print("LOSE MATCH");
@@ -33,7 +37,17 @@ public class GameManager : PersistentSingleton<GameManager>
 
     public void RestartMatch()
     {
+        if (!_canRestart) return;
+        _canRestart = false;
+        SceneManager.sceneLoaded += EnableRestarts;
+        
         OnRestartMatch.Invoke();
         LevelStateManager.Instance.ResetLevel(delayBeforeCheckpointRestart);
+    }
+
+    private void EnableRestarts(Scene _, LoadSceneMode __)
+    {
+        _canRestart = true;
+        SceneManager.sceneLoaded -= EnableRestarts;
     }
 }
