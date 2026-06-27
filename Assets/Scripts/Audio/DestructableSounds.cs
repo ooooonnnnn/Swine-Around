9c5cd6ca58776a1f9eb8a1eecd4edd47994770b1
CutebatDev <alexaverten@gmail.com>
2026-06-27 20:11:56 +0300

    juicee

--- Assets/Scripts/Audio/DestructableSounds.cs
+++ Assets/Scripts/Audio/DestructableSounds.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class DestructableSounds : MonoBehaviour
 {
     [SerializeField] private EventReference breakSound;
+    [SerializeField] private EventReference breakSoundSecondary;
     public void PlayBreakSound()
     {
         AudioManager.Instance.PlayOneShot(breakSound, transform.position);
+        AudioManager.Instance.PlayOneShot(breakSoundSecondary, transform.position);
     }
     
 }
