e508d91ec46862f78a6b48b56e0329b1da6e4dc2
CutebatDev <alexaverten@gmail.com>
2026-07-05 19:40:35 +0300

    Random food + SO

---
+++ Assets/ScriptableObject/FoodObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "FoodObject", menuName = "Food")]
+public class FoodObject : ScriptableObject
+{
+    [SerializeField] public GameObject mesh;
+    [SerializeField] public int score;
+}
---
+++ Assets/Scripts/Gameplay/Food/FoodSetup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Gameplay.Food
+{
+    public class FoodSetup : MonoBehaviour
+    {
+        [SerializeField] private FoodObject[] FoodScriptableObjects;
+        [SerializeField] private MagnetizableFood foodScript;
+        [SerializeField] private Transform root;
+        [SerializeField] private MeshRenderer previewMeshRenderer;
+        
+
+        private void Awake()
+        {
+            previewMeshRenderer.enabled = false;
+            int currentFoodIndex = Random.Range(0, FoodScriptableObjects.Length);
+
+            Instantiate(FoodScriptableObjects[currentFoodIndex].mesh, root);
+            foodScript.foodValue = FoodScriptableObjects[currentFoodIndex].score;
+        }
+    }
+}
