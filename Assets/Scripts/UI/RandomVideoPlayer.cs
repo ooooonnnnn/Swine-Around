20259f95d3fae3c746dd5b48128d2f35197a1948
CutebatDev <alexaverten@gmail.com>
2026-07-07 00:19:57 +0300

    audio guard fix + :)

--- Assets/Scripts/UI/RandomVideoPlayer.cs
+++ Assets/Scripts/UI/RandomVideoPlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using FMODUnity;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,6 +10,7 @@ public class RandomVideoPlayer : MonoBehaviour
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private VideoClip[] clips;
     public UnityEvent OnVideoStartPlaying;
+    public UnityEvent OnVideoEndPlaying;
 
     private void OnValidate()
     {
@@ -23,5 +25,12 @@ public class RandomVideoPlayer : MonoBehaviour
         videoPlayer.clip = clips[UnityEngine.Random.Range(0, clips.Length)];
         videoPlayer.time = 0;
         videoPlayer.Play();
+        StartCoroutine(ExecuteAfterTime(videoPlayer.clip.length));
+    }
+    IEnumerator ExecuteAfterTime(double time)
+    {
+        yield return new WaitForSeconds((float)time);
+
+        OnVideoEndPlaying.Invoke();
     }
 }
