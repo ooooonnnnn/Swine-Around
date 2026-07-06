59526dff3df84e8d118657c0ccf83f3a98b58908
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-07-06 17:21:48 +0300

    added suspicion states and animation

--- Assets/Scripts/Animation/PolicemanAnimatorInterface.cs
+++ Assets/Scripts/Animation/PolicemanAnimatorInterface.cs
@@ -8,7 +8,9 @@ public class PolicemanAnimatorInterface : MonoBehaviour
     [SerializeField] private Animator animator;
     private static readonly int WALK_TRIGGER_ID = Animator.StringToHash("WalkTrigger");
     private static readonly int CHASE_TRIGGER_ID = Animator.StringToHash("ChaseTrigger");
+    private static readonly int SUSPICION_TRIGGER_ID = Animator.StringToHash("SuspicionTrigger");
     private static readonly int SPEED_PARAMETER_ID = Animator.StringToHash("Speed");
+    private static readonly int SUSPICION_PARAMETER_ID = Animator.StringToHash("SuspicionPercent");
 
     public void HandleStateChanged(IPoliceState newState)
     {
@@ -27,9 +29,14 @@ public class PolicemanAnimatorInterface : MonoBehaviour
 
         if (stateType == typeof(PoliceSuspicionState))
         {
-            
+            animator.SetTrigger(SUSPICION_TRIGGER_ID);
+            return;
         }
-        
+    }
+
+    public void HandleSuspicionChanged(float suspicionPercentage)
+    {
+        animator.SetFloat(SUSPICION_PARAMETER_ID, suspicionPercentage);
     }
 
     private void FixedUpdate()
