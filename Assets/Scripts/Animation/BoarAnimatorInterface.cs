9cd6c6538de1a63879b38040ae5ab3ed19658fd1
CutebatDev <alexaverten@gmail.com>
2026-07-07 13:19:53 +0300

    Fin i think?

---
+++ Assets/Scripts/Animation/BoarAnimatorInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class BoarAnimatorInterface : MonoBehaviour
+{
+    [SerializeField] public CharacterController characterController;
+    [SerializeField] public Animator animator;
+    private static readonly int WALK_TRIGGER_ID = Animator.StringToHash("MoveTrigger");
+    private static readonly int IDLE_TRIGGER_ID = Animator.StringToHash("IdleTrigger");
+
+    private void Update()
+    {
+        if (characterController.velocity != Vector3.zero)
+            animator.SetTrigger(WALK_TRIGGER_ID);
+        else    
+            animator.SetTrigger(IDLE_TRIGGER_ID);
+    }
+}
