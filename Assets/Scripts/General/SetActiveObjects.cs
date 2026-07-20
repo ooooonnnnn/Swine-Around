6d790a302a957355b47e5960c3be86694aa2ff95
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-07-20 21:58:49 +0300

    checkpoint model switches

---
+++ Assets/Scripts/General/SetActiveObjects.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace General
+{
+    public class SetActiveObjects : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] objectsToSetActive;
+        [SerializeField] private bool[] initialStates;
+
+        private void OnValidate()
+        {
+            initialStates = new bool[objectsToSetActive.Length];
+            for (int i = 0; i < objectsToSetActive.Length; i++)
+            {
+                initialStates[i] = objectsToSetActive[i].activeSelf;
+            }
+        }
+
+        public void SetActives(bool state)
+        {
+            for (int i = 0; i < objectsToSetActive.Length; i++)
+            {
+                objectsToSetActive[i].SetActive(state ^ initialStates[i]);
+            }
+        }
+    }
+}
